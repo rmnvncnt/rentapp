be628f62a2035acb738c3118f7e47a422bb66b55
romain <romv740@gmail.com>
2016-12-06 17:46:03 +0900

    bugfix

--- rentapp.py
+++ rentapp.py
@@ -82,7 +82,6 @@ def scraping():
     url = request.form['url']
     
     # scraping
-    """
     try:
         if 'pap' in url:
             crawler = PapCrawler(url)
@@ -90,8 +89,7 @@ def scraping():
             raise Exception()
     except:
         return redirect(url_for('index'))
-    """
-    crawler = PapCrawler(url)
+
     # get data from crawl
     session['item'] = crawler.__dict__
     return redirect(url_for('validation'))
